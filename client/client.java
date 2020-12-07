ec52297a1bea00d471d922eb7d3bbb8332579c17
Aaron Bager <kingjojo3@gmail.com>
2020-12-07 17:11:42 -0500

    uploading chester's changes

--- client/client.java
+++ client/client.java
@@ -1,123 +1,146 @@
-package findHuman.client;
+import java.awt.Checkbox;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-import javax.swing.*;
-import java.util.concurrent.ExecutionException;
+public class client{
+    private static InetAddress host;
+    private static final int PORT = 1236;
 
-public class client {
+    public static void main(String[] args)
+    {
+        Socket server = null;
+        String message, command, response, serverName = "";
+        int clientPort;
+        int MAXIMUM_HAND_SIZE = 7;
 
-    //private int playNumber = 0;
-    private static final String DEFAULT_SERVER_ADDRESS = "localhost";
-    private static final int DEFAULT_SERVER_PORT = 44444;
-    private clientModel model;
-    private clientView view;
-    private int serverPort;
-    private String serverAddress;
+        Scanner serverInput = null;
+        PrintWriter serverOutput = null;
 
-
-    /* */
-    public client(String serverAddress, int serverPort) {
-        this.serverAddress = serverAddress;
-        this.serverPort = serverPort;
-    }
-
-    /* */
-    public void startConnection() {
-        System.out.println("Starting Find-the-Humans client\nServer address: " + serverAddress + "\nServer port: " + serverPort);
-        model = new clientModel(serverAddress, serverPort, this);
-        view = new clientView(this);
-        getServerMessage();
-    }
-
-    public clientModel getClientModel() {
-        return this.model;
-    }
-
-    private void getServerMessage() {
-        SwingWorker swingWorker = new SwingWorker<String, String>() {
-            @Override
-            public String doInBackground() throws Exception {
-                return model.getServerMessage();
-            }
-
-            @Override
-            public void done() {
-                try {
-                    updateView(get());
-                } catch (InterruptedException | ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        swingWorker.execute();
-    }
-
-    private void updateView (String serverMessage) {
-        String[] serverMessageComponents = serverMessage.split("--");
-        switch (serverMessageComponents[1]){
-            // Welcome & prompt player name
-            case "WELCOME":
-        //        view.showWelcomePanel();
-                getServerMessage();
-                break;
-            case "GETNAME":
-              //  view.getHumanName();
-                getServerMessage();
-                break;
-
-            case "PANELTYPE":
-                switch (serverMessageComponents[2]){
-                    case "GUESS":
-                        view.addHumanPanel("GUESS");
-                        getServerMessage();
-                        break;
-                    case "HIDE" :
-                        view.addHumanPanel("HIDE");
-                        getServerMessage();
-                        break;
-                }
-
-            case "NEWROUND":
-               // switch ()
-        }
-
-    }
+        Scanner userEntry = new Scanner(System.in);
+        boolean nameAccepted = false;
+        String username = "";
 
 
+        try {
+            do {
 
+                System.out.println("Welcome to Cards Against Humanity Online");
 
-        public static void main(String[] args) {
-        String serverAddress = DEFAULT_SERVER_ADDRESS;
-        int serverPort = DEFAULT_SERVER_PORT;
-        for (int i = 0; i < args.length; i += 2) {
-            String option = args[i];
-            String argument = null;
-            try {
-                argument = args[i + 1];
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.err.println("Options: [-a serverAddress] [-p serverPort]");
-                System.exit(1);
-            }
-            switch (option) {
-                case "-a":
-                    serverAddress = argument;
-                    break;
-                case "-p":
-                    try {
-                        serverPort = Integer.parseInt(argument);
-                    } catch (NumberFormatException e) {
-                        System.err.println("Server port must be an integer");
-                        System.exit(1);
+                message = userEntry.nextLine();
+                StringTokenizer tokens = new StringTokenizer(message);
+                command = tokens.nextToken();
+
+                // user will send "JOIN" with the IP and port info
+                if(command.equals("JOIN"))
+                {
+                    if (server != null)
+                    {
+                        System.out.println("A connection with the game has already been establisheed");
+                        continue;
                     }
-                    break;
-                default:
-                    System.err.println("Options: [-a serverAddress] [-p serverPort]");
-                    System.exit(1);
-                    break;
-            }
+                    serverName = tokens.nextToken();
+                    clientPort = Integer.parseInt(tokens.nextToken());
+                    server = new Socket("localhost", 1234);
+
+                    serverInput = new Scanner(server.getInputStream());
+                    serverOutput = new PrintWriter(server.getOutputStream(), true);
+
+                    System.out.println("Connection with " + serverName + " has been established");
+
+
+					System.out.println("Please enter a username...");
+                    // while we haven't had a name accepted by the server
+                    while(nameAccepted == false) {
+                        // get input
+                        username = userEntry.nextLine();
+                        // send to server
+                        serverOutput.println(username);
+
+                        // check available
+                        String serverResponse = serverInput.next();
+                        if(serverResponse.equals("ACCEPTED")){
+                            System.out.println("Your username has been accepted. You are: " + username);
+                            nameAccepted = true;
+                        }
+
+                        else if (serverResponse.equals("TAKEN")) {
+                            System.out.println("This username has already been taken. Please enter another username");
+                            continue;
+                        }
+
+                        else {
+                            System.out.println("An error has occurred. Please enter another username");
+                            continue;
+                        }
+                    }
+
+                    // TODO: User needs to wait until the server sends a card list for first round
+					System.out.println("Test");
+					
+                    // name stuff is done, user
+                    System.out.println("Entering game...");
+                    serverOutput.println(clientPort);
+					System.out.println("Test2");
+                    ServerSocket welcomeSocket = new ServerSocket(clientPort);
+					System.out.println("Test3");
+                    Socket dataSocket = welcomeSocket.accept();
+                    InputStream inStream = dataSocket.getInputStream();
+                    OutputStream outStream = dataSocket.getOutputStream();
+
+                    String[] hand;
+                    String currentBlackCard;
+                    String cardTzar;
+                    int cardSelection = 0;
+                    ObjectInputStream objectIn = new ObjectInputStream(inStream);
+                    Scanner scanIn = new Scanner(inStream);
+
+                    // since we don't have "commands" like in the last project
+                    // we will want to do a big while loop that the game stuff runs in instead
+                    do {
+						/* read in and print players hand */
+						System.out.println("Getting your hand...");
+						hand = (String[])objectIn.readObject();
+						int num = 0;
+						System.out.println("Your hand: ");
+						for(int i=0; i<MAXIMUM_HAND_SIZE; i++){
+							num = i+1;
+							System.out.println(num+". " + hand[i]);
+						}
+
+						/* conditional for if you are tzar or a player */
+						System.out.println("Getting card tzar...");
+						cardTzar = scanIn.nextLine();
+						
+						/* if you are card tzar */
+						if(cardTzar.equals(username)){
+							System.out.println("You are the card tzar this turn!");
+							System.out.println("Getting your black card...");
+							currentBlackCard = scanIn.nextLine();
+							System.out.println(currentBlackCard);
+							System.out.println("Awaiting submissions...");
+
+						}
+						/* you are playing this turn */
+						else{
+							System.out.println(cardTzar + " is the card tzar this turn");
+							System.out.print("Getting the black card for this turn...");
+							currentBlackCard = scanIn.nextLine();
+							System.out.println(currentBlackCard);
+
+							while(cardSelection<=0 || cardSelection>7){
+								System.out.println("Submit your white card now");
+								cardSelection = userEntry.nextInt();
+							}
+						}
+                    } while(true);
+                }
+
+
+            } while (!userEntry.nextLine().equalsIgnoreCase("QUIT"));
         }
-        client controller = new client(serverAddress, serverPort);
-        controller.startConnection();
+        catch (Exception e) {
+            // TODO: handle exception
+        }
     }
-
-
 }
